b8c27ff373899dded7889541ecde801c39478152
Matrix <timberlake2025@gmail.com>
2023-06-11 11:40:33 -0700

    testing

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,7 +7,7 @@ import io.ktor.server.application.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello Kotlin!!!!!")
+            call.respondText("Test works")
         }
     }
 }
